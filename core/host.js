bac5831d74eb74b2fa21d38bc8225f6bbddfd06c
jtparrett <jtparrett@me.com>
2018-12-15 14:48:49 +0000

    initial commit

---
+++ core/host.js
@@ -0,0 +1,87 @@
+const WebSocket = require('ws')
+const uuid = require('uuid/v1')
+const SHA256 = require('crypto-js/sha256')
+const Realm = require('realm')
+const {defaultPort} = require('../config')
+
+const [_, root, port = defaultPort, ...bootHosts] = process.argv
+let peers = {}
+
+const MessageSchema = {
+  name: 'Message',
+  primaryKey: 'id',
+  properties: {
+    id: 'string',
+    text: 'string'
+  }
+}
+
+const writeMessage = async (id, text) => {
+  const realm = await Realm.open({
+    schema: [MessageSchema]
+  })
+
+  return realm.write(() => {
+    realm.create('Message', {id, text})
+  })
+}
+
+const findMessage = async (id) => {
+  const realm = await Realm.open({
+    schema: [MessageSchema]
+  })
+
+  return realm.objects('Message').filtered('id = $0', id)
+}
+
+const broadcastToAllPeers = (peers, message) => {
+  Object.values(peers).forEach((connection) => {
+    if(connection.readyState === WebSocket.OPEN){
+      connection.send(message) 
+    }
+  })
+}
+
+const addPeerConnection = (connection) => {
+  const id = uuid()
+
+  connection.on('message', async (message) => {
+    const text = message.toString()
+    const id = SHA256(text).toString()
+    
+    const [messageExists] = await findMessage(id)
+
+    console.log(text)
+
+    if(!!messageExists) return false
+
+    try {
+      await writeMessage(id, text)
+      broadcastToAllPeers(peers, text)
+    } catch(err){
+      console.log('error writing new message')
+    }
+  })
+
+  const removePeer = () => {
+    const {[id]: _, ...rest} = peers
+    peers = rest
+  }
+
+  connection.on('close', removePeer)
+  connection.on('error', removePeer)
+
+  peers = {
+    ...peers,
+    [id]: connection
+  }
+}
+
+bootHosts.map((host) => {
+  const client = new WebSocket(host)
+  addPeerConnection(client)
+})
+
+const server = new WebSocket.Server({ port })
+
+server.on('connection', addPeerConnection)
